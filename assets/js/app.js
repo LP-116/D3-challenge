265735bdfe5940c82a647e6dc6480acb46bac0f4
Leanne <leanne_porter@hotmail.com>
2021-08-28 12:46:04 +1000

    updating code

--- assets/js/app.js
+++ assets/js/app.js
@@ -58,15 +58,15 @@ function updateToolTip(chosenXAxis, circlesGroup) {
     var label;
 
     if (chosenXAxis === "poverty") {
-        label = "In Poverty %";
+        label = "Poverty";
     }
 
     else if (chosenXAxis === "age") {
-        label = "Age (Median)";
+        label = "Age";
     }
 
     else {
-        label = "Household Income (Median)"
+        label = "Household Income"
     }
 
     var toolTip = d3.tip()
@@ -101,6 +101,54 @@ d3.csv("data.csv").then(function(stateData, err) {
             .domain([4, d3.max(stateData, d => d.healthcare)])
             .range([height, 0]);
 
+        var bottomAxis = d3.axisBottom(xLinearScale);
+        var leftAxis = d3.axisLeft(yLinearScale);
+        
+        var xAxis = chartGroup.append("g")
+            .classed("x-axis", true)
+            .attr("transform", `translate(0, ${height})`)
+            .call(bottomAxis);
+        
+        chartGroup.append("g")
+            .call(leftAxis);
+
+        var circlesGroup = chartGroup.selectAll("circle")
+            .data(stateData)
+            .enter()
+            .append("circle")
+            .attr("cx", d => xLinearScale(d.poverty))
+            .attr("cy", d => yLinearScale(d.healthcare))
+            .attr("r", "15")
+            .attr("fill", "dodgerblue")
+            .attr("opacity", ".50")
+            .attr("stroke","blue")
+            .attr("stroke-width", 2);
+            
+      
+        var circleLabels = chartGroup.selectAll(null)
+              .data(stateData)
+              .enter()
+              .append("text")
+              .attr("x", d => xLinearScale(d.poverty))
+              .attr("y", d => yLinearScale(d.healthcare))
+              .text(d => d.abbr)
+              .attr("font-size", "11px")
+              .attr("text-anchor", "middle")
+              .attr("alignment-baseline", "middle")
+              .attr("fill", "white");
+
+        var labelsGroup = chartGroup.append("g")
+            .attr("transform", `translate(${width /2}, ${heigh + 20})`);
+
+        var povertyLabel = labelsGroup.append("text")
+            .attr("x", 0)
+            .attr("y", 20)
+            .attr("value", "poverty")
+            .classed("active", true)
+            .text("In Poverty (%)");
+
+            
+
         
 
 
@@ -120,95 +168,4 @@ d3.csv("data.csv").then(function(stateData, err) {
 
 
 
-
-
-d3.csv("data.csv").then(function(stateData) {
-
-    stateData.forEach(function(data) {
-        data.poverty = +data.poverty;
-        data.healthcare = +data.healthcare;
-    });
-
-    var xLinearScale = d3.scaleLinear()
-      .domain([8, d3.max(stateData, d => d.poverty)])
-      .range([0, width]);
-
-    var yLinearScale = d3.scaleLinear()
-      .domain([4, d3.max(stateData, d => d.healthcare)])
-      .range([height, 0]);
-
-    var bottomAxis = d3.axisBottom(xLinearScale);
-    var leftAxis = d3.axisLeft(yLinearScale);
-
-    chartGroup.append("g")
-      .attr("transform", `translate(0, ${height})`)
-      .call(bottomAxis);
-
-    chartGroup.append("g")
-      .call(leftAxis);
-
-    
-    var circlesGroup = chartGroup.selectAll("circle")
-      .data(stateData)
-      .enter()
-      .append("circle")
-      .attr("cx", d => xLinearScale(d.poverty))
-      .attr("cy", d => yLinearScale(d.healthcare))
-      .attr("r", "15")
-      .attr("fill", "dodgerblue")
-      .attr("opacity", ".50")
-      .attr("stroke","blue")
-      .attr("stroke-width", 2);
-      
-
-    var circleLabels = chartGroup.selectAll(null)
-        .data(stateData)
-        .enter()
-        .append("text")
-        .attr("x", d => xLinearScale(d.poverty))
-        .attr("y", d => yLinearScale(d.healthcare))
-        .text(d => d.abbr)
-        .attr("font-size", "11px")
-        .attr("text-anchor", "middle")
-        .attr("alignment-baseline", "middle")
-        .attr("fill", "white");
-
-    var toolTip = d3.tip()
-        .attr("class", "tooltip")
-        .offset([10, -10])
-        .html(function(d) {
-            return (`${d.state}<br>Poverty: ${d.poverty}<br>Healthcare: ${d.healthcare}`);
-        });
-
-    chartGroup.call(toolTip);
-
-    circlesGroup.on("mouseover", function(data) {
-        toolTip.show(data, this);
-    })
-
-        .on("mouseout", function(data, index) {
-            toolTip.hide(data);
-        });
- 
-
-     var axisLabels = chartGroup.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left + 40)
-      .attr("x", 0 - (height / 2))
-      .attr("dy", "1em")
-      .attr("class", "axisText")
-      .text("Lacks Healthcare %");
-
-
-     var chartTitle = chartGroup.append("text")
-      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-      .attr("class", "axisText")
-      .text("In Poverty %");
-  }).catch(function(error) {
-    console.log(error);
-
-
-
-
-});
 
